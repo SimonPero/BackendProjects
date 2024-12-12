e780fece3469f45b84ea4dbf6776b5ef35badcad
Simon Pero <simonraulpero@gmail.com>
2024-12-12 17:23:11 -0300

    MInor change

--- note-takin-app/front/app/user/register/page.tsx
+++ note-takin-app/front/app/user/register/page.tsx
@@ -14,6 +14,7 @@ import { Input } from "@/components/ui/input";
 import { useLanguage } from "@/contexts/LanguageContext";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 
@@ -71,8 +72,9 @@ export default function Page() {
     },
   });
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    registerUser(values);
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    await registerUser(values);
+    redirect("/user/login")
   }
 
   return (
